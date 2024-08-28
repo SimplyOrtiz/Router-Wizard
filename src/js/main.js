586a989de9172f484139f301229538805d38db59
Ortiz <guilherme.ortiz.franca@escola.pr.gov.br>
2024-08-28 22:27:49 +0000

    Created Basic Website Logic

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,9 @@
-//0Roteador, 1Redes WI-FI, 2Segurança Das Redes WI-FI, 3Canais WI-FI, 4Tipos de Redes e conexões
+//Bagulho
+//0 Roteador
+//1 Redes WI-FI
+//2 Segurança Das Redes WI-FI
+//3 Canais WI-FI
+//4 Tipos de Redes e conexões
 
 const pageTitle = [
     "Roteador",
@@ -16,4 +21,10 @@ const pageTexts = [
     "Rede de área local (LAN) Uma LAN é um sistema interconectado limitado em tamanho e geografia. ...<br>Redes de longa distância (WAN) ...<br></br>Redes de provedores de serviços. ...<br></br>Redes em nuvem.<br>Uma rede empresarial que abrange edifícios, cidades e até países é chamada de rede de longa distância (WAN). Enquanto as redes de área locais são usadas para transmitir dados em velocidades mais altas em estreita proximidade, as WANs são configuradas para comunicação de longa distância que é segura e confiável."
 ];
 
-console.log(pageTexts[1])
+function ChangePage(titleNumber,textNumber){
+    var h1Title = document.getElementById("topic-title");
+    var pText = document.getElementById("text-paragraph");
+
+    h1Title.innerHTML = pageTitle[titleNumber]
+    pText.innerHTML = pageTexts[textNumber]
+}
